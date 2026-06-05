16b39a202916af4f6c933d265181c78919492d8d
Christian Tietze <me@christiantietze.de>
2026-06-05 15:41:03 +0200

    test: asset deduplication setting

--- TextBundleTests/TextBundleTests.m
+++ TextBundleTests/TextBundleTests.m
@@ -164,15 +164,52 @@
     XCTAssertEqualObjects(filename, @"sample asset 2.jpg");
     XCTAssertEqual(tb.assetsFileWrapper.fileWrappers.count, 2);
 
-    // Adding a different file with the same name should update the new name
+    // Adding the same file with different filewrapper should duplicate it with a new name
+    NSFileWrapper *sameContentFileWrapper = [[NSFileWrapper alloc] initWithURL:assetURL options:0 error:nil];
+    filename = [tb addAssetFileWrapper:sameContentFileWrapper];
+    XCTAssertEqualObjects(filename, @"sample asset 3.jpg");
+    XCTAssertEqual(tb.assetsFileWrapper.fileWrappers.count, 3);
+
+    // Adding a different file with the same name should duplicate with new name
     NSURL *assetURL2 = [[NSBundle bundleForClass:[self class]] URLForResource:@"sample asset 2" withExtension:@"jpg"];
     NSFileWrapper *assetFileWrapper2 = [[NSFileWrapper alloc] initWithURL:assetURL2 options:0 error:nil];
-    
     assetFileWrapper2.filename = @"sample asset.jpg";
     assetFileWrapper2.preferredFilename = @"sample asset.jpg";
     filename = [tb addAssetFileWrapper:assetFileWrapper2];
-    XCTAssertEqualObjects(filename, @"sample asset 3.jpg");
-    XCTAssertEqual(tb.assetsFileWrapper.fileWrappers.count, 3);
+    XCTAssertEqualObjects(filename, @"sample asset 4.jpg");
+    XCTAssertEqual(tb.assetsFileWrapper.fileWrappers.count, 4);
+}
+
+- (void)testAddAssetsPreventingDuplication
+{
+    NSURL *fileURL = [self textBundleURLForFilename:@"only text"];
+    NSError *e = nil;
+    TextBundleWrapper *tb = [[TextBundleWrapper alloc] initWithUrl:fileURL options:NSFileWrapperReadingImmediate error:&e];
+    // Enable deduplication
+    tb.preventAssetDuplication = YES;
+
+    NSURL *assetURL = [[NSBundle bundleForClass:[self class]] URLForResource:@"sample asset" withExtension:@"jpg"];
+    NSFileWrapper *assetFileWrapper = [[NSFileWrapper alloc] initWithURL:assetURL options:0 error:nil];
+
+    // Adding the first time should add the file to the bundle
+    NSString *filename = [tb addAssetFileWrapper:assetFileWrapper];
+    XCTAssertEqualObjects(filename, @"sample asset.jpg");
+    XCTAssertEqual(tb.assetsFileWrapper.fileWrappers.count, 1);
+
+    // Adding the same name with the same data again should reuse the existing file, not duplicate it
+    NSFileWrapper *sameContentFileWrapper = [[NSFileWrapper alloc] initWithURL:assetURL options:0 error:nil];
+    filename = [tb addAssetFileWrapper:sameContentFileWrapper];
+    XCTAssertEqualObjects(filename, @"sample asset.jpg");
+    XCTAssertEqual(tb.assetsFileWrapper.fileWrappers.count, 1);
+
+    // Adding a different file with the same name should still rename, even with deduplication on
+    NSURL *assetURL2 = [[NSBundle bundleForClass:[self class]] URLForResource:@"sample asset 2" withExtension:@"jpg"];
+    NSFileWrapper *assetFileWrapper2 = [[NSFileWrapper alloc] initWithURL:assetURL2 options:0 error:nil];
+    assetFileWrapper2.filename = @"sample asset.jpg";
+    assetFileWrapper2.preferredFilename = @"sample asset.jpg";
+    filename = [tb addAssetFileWrapper:assetFileWrapper2];
+    XCTAssertEqualObjects(filename, @"sample asset 2.jpg");
+    XCTAssertEqual(tb.assetsFileWrapper.fileWrappers.count, 2);
 }
 
 - (void)testAssetLookup
